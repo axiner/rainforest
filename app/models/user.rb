980e9be781a5ec99355ba64d697f14ce1a46dd6d
axiner <axiner.w@gmail.com>
2016-04-15 00:12:23 -0400

    3rd round

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ActiveRecord::Base
   has_secure_password
+  has_many :reviews
+  has_many :products, through: :reviews
+
+  private
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation, :name)
+  end
 end
